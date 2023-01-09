759cdec2bdeadd6d00949194d25002fe10a938ff
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-01-09 21:39:39 +0500

    database config

--- dataAccessLayer/DatabaseConnection.js
+++ dataAccessLayer/DatabaseConnection.js
@@ -1,11 +1,11 @@
-const mysql=require("mysql");
-const pool =mysql.createPool({
-host:"localhost",
-user:"root",
-password:"root",
-database:"Portfolio",
-connectionLimit:10,
-
-})
-
-module.exports= pool;
+const mysql = require("mysql");
+require("dotenv").config();
+const pool = mysql.createPool({
+  host: process.env.Database_url,
+  user: process.env.Database_user,
+  password: process.env.Database_password,
+  database: process.env.Database_name,
+  port: process.env.Database_port,
+  connectionLimit: 10,
+});
+module.exports = pool;
