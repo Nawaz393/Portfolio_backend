7a3db3b3415089e8976b1b974d1e69abb0c8126a
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-01-21 02:51:45 +0500

    fixing a minor bug

--- controller/handelRegister/RegisterSchema.js
+++ controller/handelRegister/RegisterSchema.js
@@ -1,20 +1,10 @@
-const joi =require("joi");
+const joi = require("joi");
 
+const schema = joi.object().keys({
+  userName: joi.string().min(5).max(12).required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+  role: joi.string().min(5),
+});
 
-const schema=joi.object().keys(
-    {
-
-userName:joi.string().min(8).max(12).required(),
-email:joi.string().email().required(),
-password:joi.string().min(8).required(),
-role:joi.string().min(5),
-
-
-
-    }
-
-
-);
-
-module.exports=schema;
-
+module.exports = schema;
